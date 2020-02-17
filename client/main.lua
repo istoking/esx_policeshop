4b458a31c5cbe11c044ec71a9de5bdd021a98811
istoking <58592678+istoking@users.noreply.github.com>
2020-02-17 21:02:03 +1300

    Create main.lua

---
+++ client/main.lua
@@ -0,0 +1,198 @@
+local Keys = {
+	["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
+	["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
+	["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
+	["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
+	["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
+	["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
+	["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
+	["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
+	["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
+}
+
+ESX                           = nil
+local HasAlreadyEnteredMarker = false
+local LastZone                = nil
+local CurrentAction           = nil
+local CurrentActionMsg        = ''
+local CurrentActionData       = {}
+local PlayerData        = {}
+
+Citizen.CreateThread(function()
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(0)
+	end
+
+	while PlayerData.job == nil do
+		PlayerData = ESX.GetPlayerData()
+		Citizen.Wait(100)
+	end
+
+	Citizen.Wait(5000)
+
+	ESX.TriggerServerCallback('esx_policeshop:requestDBItems', function(policeshopItems)
+		for k,v in pairs(policeshopItems) do
+			Config.Zones[k].Items = v
+		end
+	end)
+end)
+
+RegisterNetEvent('esx:playerLoaded')
+AddEventHandler('esx:playerLoaded', function(xPlayer)
+  PlayerData = xPlayer
+end)
+
+RegisterNetEvent('esx:setJob')
+AddEventHandler('esx:setJob', function(job)
+  PlayerData.job = job
+end)
+
+function OpenShopMenu(zone)
+
+	local elements = {}
+	for i=1, #Config.Zones[zone].Items, 1 do
+		local item = Config.Zones[zone].Items[i]
+
+		if item.limit == -1 then
+			item.limit = 100
+		end
+
+		table.insert(elements, {
+			label      = ('%s - <span style="color:green;">%s</span>'):format(item.label, _U('policeshop_item', ESX.Math.GroupDigits(item.price))),
+			label_real = item.label,
+			item       = item.item,
+			price      = item.price,
+
+			-- menu properties
+			value      = 1,
+			type       = 'slider',
+			min        = 1,
+			max        = item.limit
+		})
+	end
+
+	ESX.UI.Menu.CloseAll()
+	ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'policeshop', {
+		title    = _U('policeshop'),
+		align    = 'bottom-right',
+		elements = elements
+	}, function(data, menu)
+		ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'policeshop_confirm', {
+			title    = _U('policeshop_confirm', data.current.value, data.current.label_real, ESX.Math.GroupDigits(data.current.price * data.current.value)),
+			align    = 'bottom-right',
+			elements = {
+				{label = _U('no'),  value = 'no'},
+				{label = _U('yes'), value = 'yes'}
+			}
+		}, function(data2, menu2)
+			if data2.current.value == 'yes' and PlayerData.job.name == 'police' then
+				TriggerServerEvent('esx_policeshop:buyItem', data.current.item, data.current.value, zone)
+			end
+
+			menu2.close()
+		end, function(data2, menu2)
+			menu2.close()
+		end)
+	end, function(data, menu)
+		menu.close()
+
+		CurrentAction     = 'policeshop_menu'
+		CurrentActionMsg  = _U('press_menu')
+		CurrentActionData = {zone = zone}
+	end)
+end
+
+AddEventHandler('esx_policeshop:hasEnteredMarker', function(zone)
+	CurrentAction     = 'policeshop_menu'
+	CurrentActionMsg  = _U('press_menu')
+	CurrentActionData = {zone = zone}
+end)
+
+AddEventHandler('esx_policeshop:hasExitedMarker', function(zone)
+	CurrentAction = nil
+	ESX.UI.Menu.CloseAll()
+end)
+
+function Draw3DText(x,y,z, text)
+    local onScreen,_x,_y=World3dToScreen2d(x,y,z)
+    local px,py,pz=table.unpack(GetGameplayCamCoords())
+        
+    SetTextScale(0.35, 0.35)
+    SetTextFont(4)
+    SetTextProportional(1)
+    SetTextColour(255, 255, 255, 215)
+    SetTextEntry("STRING")
+    SetTextCentre(1)
+    AddTextComponentString(text)
+    DrawText(_x,_y)
+    local factor = (string.len(text)) / 370
+    DrawRect(_x,_y+0.0125, 0.015+ factor, 0.03, 41, 11, 41, 68)
+end
+-- Display markers
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+		local coords = GetEntityCoords(PlayerPedId())
+
+		for k,v in pairs(Config.Zones) do
+			for i = 1, #v.Pos, 1 do
+				if(Config.Type ~= -1 and GetDistanceBetweenCoords(coords, v.Pos[i].x, v.Pos[i].y, v.Pos[i].z, true) < Config.DrawDistance) then
+					DrawMarker(Config.Type, v.Pos[i].x, v.Pos[i].y, v.Pos[i].z - 0.99, 0, 0, 0, 0, 0, 0, 1.5, 1.5, 1.0, 139, 16, 20, 250, false, false, 2, true, false, false, false)
+					DrawMarker(29, v.Pos[i].x, v.Pos[i].y, v.Pos[i].z + 0.25, 0, 0, 0, 0, 0, 0, 0.5, 0.5, 0.5, 139, 16, 20, 250, false, false, 2, true, false, false, false)
+				end
+			end
+		end
+	end
+end)
+
+-- Enter / Exit marker events
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+		local coords      = GetEntityCoords(PlayerPedId())
+		local isInMarker  = false
+		local currentZone = nil
+
+		for k,v in pairs(Config.Zones) do
+			for i = 1, #v.Pos, 1 do
+				if(GetDistanceBetweenCoords(coords, v.Pos[i].x, v.Pos[i].y, v.Pos[i].z, true) < Config.Size.x) then
+					isInMarker  = true
+					policeshopItems   = v.Items
+					currentZone = k
+					LastZone    = k
+					Draw3DText(v.Pos[i].x, v.Pos[i].y, v.Pos[i].z + 0.5, '~w~Press ~g~[E] ~w~to access the Store')
+				end
+			end
+		end
+		if isInMarker and not HasAlreadyEnteredMarker then
+			HasAlreadyEnteredMarker = true
+			TriggerEvent('esx_policeshop:hasEnteredMarker', currentZone)
+		end
+		if not isInMarker and HasAlreadyEnteredMarker then
+			HasAlreadyEnteredMarker = false
+			TriggerEvent('esx_policeshop:hasExitedMarker', LastZone)
+		end
+	end
+end)
+
+-- Key Controls
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+
+		if CurrentAction ~= nil then
+--			ESX.ShowHelpNotification(CurrentActionMsg)
+
+			if IsControlJustReleased(0, Keys['E']) then
+				if CurrentAction == 'policeshop_menu' then
+					OpenShopMenu(CurrentActionData.zone)
+				end
+
+				CurrentAction = nil
+			end
+		else
+			Citizen.Wait(500)
+		end
+	end
+end)
