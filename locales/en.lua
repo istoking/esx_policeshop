abdd3147811008c7c526a3b15165bcc471413499
istoking <58592678+istoking@users.noreply.github.com>
2020-02-17 21:02:41 +1300

    Create en.lua

---
+++ locales/en.lua
@@ -0,0 +1,11 @@
+Locales['en'] = {
+  ['policeshop'] = 'Police Shop',
+  ['press_menu'] = 'press ~INPUT_CONTEXT~ to access the store.',
+  ['policeshop_item'] = '$%s',
+  ['bought'] = 'you just bought %sx %s for $%s',
+  ['not_enough'] = 'you do not have enough money, you\'re missing $%s!',
+  ['player_cannot_hold'] = 'you do not have enough free space in your inventory!',
+  ['policeshop_confirm'] = 'buy %sx %s for $%s?',
+  ['no'] = 'no',
+  ['yes'] = 'yes',
+}
